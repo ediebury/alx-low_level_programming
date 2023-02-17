ed986f3a94b96de9d344ce0801d8ee9be92b0b4b
Your Name <you@example.com>
2023-02-17 05:17:07 -0800

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all single digit numbers of base 10 starting from 0
+ * main - program that prints all single digit numbers of
+ * base 10 starting from 0
  * Return: 0
  */
 
