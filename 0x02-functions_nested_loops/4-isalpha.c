28686bfd6dd009e9bac68297c126c8dd41bd7d80
Your Name <you@example.com>
2023-02-21 06:55:34 -0800

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
+ * _isalpha - Check if a character is alphabet
+ * @c: the character to be check
+ * Return: 1 if charater is letter, lowercase or upper, o upper
  */
-
-int _islower(int c)
+int _isalpha(int c)
 {
-
-	if (c >= 97 && c <= 122)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
