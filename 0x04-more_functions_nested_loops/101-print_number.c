ae1ae52f6f7ee7224957e73124ea0be245978488
Your Name <you@example.com>
2023-02-23 15:44:37 -0800

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,22 +2,33 @@
 
 /**
  * print_number - prints an integer
- * @n: .input integer parameter
- *
+ * @n: param
  */
-
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int i, j, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		i = -1;
+		i = n * -1;
 	}
-	if (i / 10)
+	else
 	{
-		print_number(i / 10);
+		i = n;
 	}
-	_putchar(i % 10 + '0');
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
 }
