6edca12faf5817d07879acfc20564859c39eaf6e
Your Name <you@example.com>
2023-03-10 15:58:14 -0800

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the number of arguments passed into it
+ * main - program that prints all arguments it receives
  * @argc: argument count
  * @argv: argunment vector
  *
@@ -11,10 +11,15 @@
 
 int main(int argc, char *argv[])
 {
+	int count = 0;
+
 	if (argc > 0)
 	{
-		argv = argv;
-		printf("%d\n", argc - 1);
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
 	}
 	return (0);
 }
