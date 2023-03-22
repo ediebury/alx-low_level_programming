e3c93309305afee1804d327540c46261aac722c0
Your Name <you@example.com>
2023-03-22 11:06:13 -0700

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,36 @@
+#include <stdio.h>
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
- */
-int (*get_op_func(char *s))(int, int)
+  *get_op_func - A function that gets operator.
+  *@s: A string specifying type of operation.
+  *Return: Returns operator function.
+  */
+int (*get_op_func(char *s)) (int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
-	int i = 0;
+
+	int i;
+
+	i = 0;
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
-		i++;
+		if (*ops[i].op == *s)
+		{
+			return (*ops[i].f);
+		}
+		else
+		{
+			++i;
+		}
 	}
-
-	return (0);
+	return (NULL);
 }
