2f1198e772e204119cc9fdbf6e1448ca1a687d4d
Your Name <you@example.com>
2023-03-22 11:10:00 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,11 @@
-#ifndef _HEADER_
-#define _HEADER_
-
+#ifndef CALC_H
+#define CALC_H
 /**
- * struct op - Struct op
- * @op:  operator
- * @f:function to be performed 
- */
+  * struct op - Struct op.
+  *
+  * @op : The operator.
+  * @f: The function associated.
+  */
 typedef struct op
 {
 	char *op;
@@ -17,6 +17,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
+int (*get_op_func(char *s)) (int, int);
 #endif
