367e356c5200753785bdf7aef42e7c31ebd9740e
Your Name <you@example.com>
2023-03-22 11:00:31 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,47 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
- */
-int main(int argc, char *argv[])
+  *main - A program that performs simple operations.
+  *@argc: Argument count.
+  *@argv: Argument vector.
+  *Return: 0 on sucessful exit.
+  */
+
+int main(int argc, char **argv)
 {
-	int (*oprt)(int, int);
+	int num1, num2, res;
+	char sign;
+	char *op;
+	int (*ope)(int, int);
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		printf("Error");
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	sign = argv[2][0];
 
-	if (!oprt)
+	if ((sign != '+' && sign != '-' && sign != '*' && sign != '/' && sign != '%') || argv[2][1] != '\0')
 	{
-		printf("Error\n");
+		printf("Error");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0 && (sign == '%' || sign == '/'))
+	{
+		printf("Error");
+		exit(100);
+	}
+
+	op = argv[2];
+	ope = get_op_func(op);
+	res = ope(num1, num2);
+	printf("%d\n", res);
 	return (0);
-}
+	}
