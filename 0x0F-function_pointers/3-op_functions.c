e7f12f585e6a79ede11855f27dfd77172e1fb291
Your Name <you@example.com>
2023-03-22 11:12:50 -0700

    update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,57 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+  *op_add - Adds two integers.
+  *@a: First integer.
+  *@b: Second integer.
+  *Return: Sum of the integers.
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+  *op_sub - Subtracts first integer from second.
+  *@a: First integer.
+  *@b: Second integer.
+  *Return: Subtraction of the integers.
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
- */
+  *op_mul - Multiplies two integers.
+  *@a: First integer.
+  *@b: Second integer.
+  *Return: Multiplication of the integers.
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
- */
+  *op_div - Divides first integer by second.
+  *@a: First integer.
+  *@b: Second integer.
+  *Return: Division of the integers.
+  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+  *op_mod - Returns the remainder of the first integer divided by the second.
+  *@a: First integer.
+  *@b: Second integer.
+  *Return: Modulus of the integers.
+  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
