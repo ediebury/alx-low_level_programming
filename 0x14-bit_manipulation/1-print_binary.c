7c25b3fb9ee3e155d23a41fda35adb78d23e7715
Your Name <you@example.com>
2023-04-10 11:19:01 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,19 @@
 #include "main.h"
+
 /**
- * create_file - create a file named filename
- * @filename: name of the file
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure (file can not be created,
- * file can not be written, write “fails”, etc…)
+ * print_binary - prints the binary representation of an unsigned long int
+ *
+ * @n: number to be printed in binary
+ *
+ * Return: void
  */
-
-int create_file(const char *filename, char *text_content)
+void print_binary(unsigned long int n)
 {
-	register int _file, _write, text_len = 0;
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
 
-	if (!(filename))
-	{
-		return (-1);
-	}
-	_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (_file == -1)
-	{
-		return (-1);
-	}
-	if (text_content)
-	{
-		while (text_content[text_len])
-		{
-			text_len++;
-		}
-		_write = write(_file, text_content, text_len);
-		if (_write == -1)
-		{
-			return (-1);
-		}
-	}
-	close(_file);
-	return (1);
+	_putchar((n & 1) + '0');
 }
+
